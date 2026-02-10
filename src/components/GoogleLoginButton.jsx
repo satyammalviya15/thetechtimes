dc236a51e30a950f7c0d1d426843fe6c85811508
Satyam Malviya <satyammalviya281@gmail.com>
2026-02-10 17:52:58 +0530

    updates

--- src/components/GoogleLoginButton.jsx
+++ src/components/GoogleLoginButton.jsx
@@ -1,6 +1,8 @@
 export const GoogleLoginButton = () => {
+    const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+
     const loginWithGoogle = () => {
-      window.open("http://localhost:5000/auth/google", "_self");
+      window.open(`${BACKEND_URL}/auth/google`, "_self");
     };
 
     return <button onClick={loginWithGoogle}>Continue with Google</button>;
